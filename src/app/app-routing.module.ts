e611e445d8236f83a88ca596a79ab9e9690705a1
Salihullah <salihabid56@yahoo.com>
2019-11-19 18:58:37 -0800

    commiting changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,12 +13,9 @@ const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'profile', component: ProfileComponent},
   { path: 'login', component: LoginComponent},
-<<<<<<< HEAD
-  { path: 'create-account', component: CreateAccountComponent}
-=======
+  { path: 'create-account', component: CreateAccountComponent},
   { path: 'nav', component: NavComponent},
   { path: 'home', component: HomeComponent},
->>>>>>> 5c2c366cd368a47079421facd5aa18ac332144fd
 ];
 
 @NgModule({
