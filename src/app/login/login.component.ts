301f24b7c7874ec52b2cf248b827b0aa148fb488
Salihullah <salihabid56@yahoo.com>
2019-12-05 19:40:42 -0800

    add redirction

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -39,15 +39,4 @@ export class LoginComponent implements OnInit {
             alert("LOGIN FAILED");
         });
     }
-
-    isUserLoggedIn() {
-        let user = sessionStorage.getItem('username')
-        console.log(!(user === null))
-        return !(user === null)
-      }
-    
-      logOut() {
-        sessionStorage.removeItem('username')
-      }
-
 }
