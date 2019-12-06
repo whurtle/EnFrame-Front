b3948a7d12e7de1f846322796e83588f15386037
Angel <amvasquez138@gmail.com>
2019-12-06 12:05:15 -0800

    Added TensorFlow
    added tensorflow to upload.component.ts file
     predicts images

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import { Prediction } from '../prediction';
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
+  imageSrc: string;
+  @ViewChild('img', {static: false}) imageEl: ElementRef;
+
+  predictions: Prediction[];
+
+  model: any;
+  loading: boolean;
+
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    //this.loading = true; 
+    console.log('loading mobilenet model...');
+    this.model = await mobilenet.load();
+    console.log('Successfully loaded model');
+    this.loading = false;
   }
+  
+  async fileChangeEvent(event) {
+
+    if(event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (res: any) => {
+        this.imageSrc = res.target.result;
+
+        setTimeout(async () => {
+          const imgEl = this.imageEl.nativeElement;
+          this.predictions = await this.model.classify(imgEl);
+        }, 0);
+      }
+    }
+  
+    
+    }
 
 }
