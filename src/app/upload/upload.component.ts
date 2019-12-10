956a4763989c48b276d275bcdbd3112722de4ff7
Aaron <ahelman@csumb.edu>
2019-12-09 21:40:58 -0800

    working image upload

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef, SystemJsNgModuleLoader } from '@angular/core';
 import * as mobilenet from '@tensorflow-models/mobilenet';
 import { Prediction } from '../prediction';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-upload',
@@ -17,7 +19,16 @@ export class UploadComponent implements OnInit {
   model: any;
   loading: boolean;
 
-  constructor() { }
+  fileData: File = null;
+  previewUrl:any = null;
+  fileUploadProgress: string = null;
+  uploadedFilePath: string = null;
+  
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) { }
 
   async ngOnInit() {
     // this.loading = true; 
@@ -32,7 +43,6 @@ export class UploadComponent implements OnInit {
       const reader = new FileReader();
 
       reader.readAsDataURL(event.target.files[0]);
-
       reader.onload = (res: any) => {
         this.imageSrc = res.target.result;
 
@@ -44,6 +54,36 @@ export class UploadComponent implements OnInit {
         }
       };
     }
+  }
+  fileProgress(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+    this.preview();
+  }
+
+  preview() {
+    // Show preview 
+    var mimeType = this.fileData.type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
     }
 
+    var reader = new FileReader();      
+    reader.readAsDataURL(this.fileData); 
+    reader.onload = (_event) => { 
+      this.previewUrl = reader.result; 
+    }
+  }
+  
+  onSubmit() {
+      const formData = new FormData();
+      formData.append('file', this.fileData);
+      console.log(this.fileData);
+      //let resp = this.http.get<boolean>("https://enflame-backend.herokuapp.com/user/isAdmin", { params : {email : user}});
+      this.http.post<string>('http://localhost:8080/photo/uploadPhoto', formData)
+        .subscribe(res => {
+          console.log(res);
+          // this.uploadedFilePath = res.data.filePath;
+          alert('SUCCESS !!');
+        }) 
+    }
 }
